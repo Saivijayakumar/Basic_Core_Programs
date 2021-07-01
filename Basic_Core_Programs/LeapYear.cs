cb456a609e946b07b5e6d4b2fd7d63cfa4eac2e0
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-01 10:49:22 +0530

    Finding LeapYear or Not

--- Basic_Core_Programs/LeapYear.cs
+++ Basic_Core_Programs/LeapYear.cs
@@ -13,9 +13,13 @@ namespace Basic_Core_Programs
             int Year;
             Console.WriteLine("Please enter a Year :");
             Year = Convert.ToInt32(Console.ReadLine());
+            //It check the given Year is valid or not like It is a four digit number or not
             if(Year >= 1000 && Year <= 9999)
             {
-                if(((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))
+                //If this condition (Year % 400 == 0) satisfy it is definatly a leap year but in 
+                //some cases the aboue condition will fail even though it is leap year then we have to use
+                //this condition (Year % 4 == 0) && (Year % 100 != 0) if it pass then it is also a leapYear
+                if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))
                 {
                     Console.WriteLine("{0} is a Leap Year",Year);
                 }
